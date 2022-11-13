7e41820476433350176b78d604b01197fac8e73e
kmmins <kmmins@yandex.ru>
2022-11-13 22:50:50 +0300

    refactor: CustomLinkedList вынесен в отдельный класс со вложенным классом Node внутри. fix: доработаны методы remove(int id) в классе InMemoryHistoryManager и getTasks() в классе CustomLinkedList.

--- src/ru/yandex/taskTracker/service/InMemoryHistoryManager.java
+++ src/ru/yandex/taskTracker/service/InMemoryHistoryManager.java
@@ -1,17 +1,16 @@
 package ru.yandex.taskTracker.service;
 
 import ru.yandex.taskTracker.model.*;
+import ru.yandex.taskTracker.util.CustomLinkedList;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
     private CustomLinkedList customLinkedListName = new CustomLinkedList();
 
-    private HashMap<Integer, Node<Task>> hashMapName = new HashMap<>();
+    private HashMap<Integer, CustomLinkedList.Node<Task>> hashMapName = new HashMap<>();
 
     @Override
     public void appendHistory(Task task) {
@@ -24,60 +23,14 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void remove(int id) {
-        if (hashMapName.containsKey(id)) {
-            customLinkedListName.removeNod(hashMapName.get(id));
-            hashMapName.remove(id);
+        CustomLinkedList.Node<Task> value = hashMapName.remove(id);
+        if (value != null) {
+            customLinkedListName.removeNod(value);
         }
     }
 
     @Override
     public List<Task> getHistoryName() {
         return customLinkedListName.getTasks();
-    }
-
-    private class CustomLinkedList {
-        public Node<Task> head;
-        public Node<Task> tail;
-
-        public void linkLast(Task task) {
-            final Node<Task> oldTail = tail;
-            final Node<Task> newNode = new Node<>(tail, task, null);
-            tail = newNode;
-            if (oldTail == null)
-                head = newNode;
-            else
-                oldTail.next = newNode;
-        }
-
-        public ArrayList<Task> getTasks() {
-            ArrayList<Task> result = new ArrayList<>();
-            if (head == null) {
-                return result;
-            } else {
-                Node<Task> nodeIter = head;
-                result.add(nodeIter.data);
-                while (nodeIter.next != null) {
-                    nodeIter = nodeIter.next;
-                    result.add(nodeIter.data);
-                }
-                return result;
-            }
-        }
-
-        public void removeNod(Node<Task> nodeX) {
-
-            final Node<Task> next = nodeX.next;
-            final Node<Task> prev = nodeX.prev;
-            if (prev == null) {
-                head = next;
-            } else {
-                prev.next = next;
-            }
-            if (next == null) {
-                tail = prev;
-            } else {
-                next.prev = prev;
-            }
-        }
     }
 }
---
+++ src/ru/yandex/taskTracker/util/CustomLinkedList.java
@@ -0,0 +1,85 @@
+package ru.yandex.taskTracker.util;
+
+import ru.yandex.taskTracker.model.Task;
+
+import java.util.ArrayList;
+
+public class CustomLinkedList {
+    public Node<Task> head;
+    public Node<Task> tail;
+
+    /**
+     * <p>Связывает задачу в CustomLinkedList </p>
+     *
+     * @param task задача полученная по id
+     */
+    public void linkLast(Task task) {
+        final Node<Task> oldTail = tail;
+        final Node<Task> newNode = new Node<>(tail, task, null);
+        tail = newNode;
+        if (oldTail == null)
+            head = newNode;
+        else
+            oldTail.next = newNode;
+    }
+
+    /**
+     * <p>Получение списка задача из отслеживаемой истории хранящейся в CustomLinkedList </p>
+     *
+     * @return список связанных задач истории просмотров
+     */
+    public ArrayList<Task> getTasks() {
+        ArrayList<Task> result = new ArrayList<>();
+        if (head == null) {
+            return result;
+        } else {
+            Node<Task> nodeIter = head;
+            while (true) {
+                result.add(nodeIter.data);
+                nodeIter = nodeIter.next;
+                if (nodeIter == null)
+                    break;
+            }
+            return result;
+        }
+    }
+
+    /**
+     * <p>Вырезает узел из CustomLinkedList </p>
+     *
+     * @param nodeX узел который необходимо вырезать из CustomLinkedList
+     */
+    public void removeNod(Node<Task> nodeX) {
+
+        final Node<Task> next = nodeX.next;
+        final Node<Task> prev = nodeX.prev;
+        if (prev == null) {
+            head = next;
+        } else {
+            prev.next = next;
+        }
+        if (next == null) {
+            tail = prev;
+        } else {
+            next.prev = prev;
+        }
+    }
+
+    /**
+     * <p>Узел, в связанном списке CustomLinkedList</p>
+     *
+     * @param <T> принимает объект типа <Task>
+     */
+    public class Node<T> {
+
+        public T data;
+        public Node<T> next;
+        public Node<T> prev;
+
+        public Node(Node<T> prev, T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
